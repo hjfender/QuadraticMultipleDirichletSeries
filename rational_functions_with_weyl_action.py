937aa57d805a802dd3bc015584f6aeebf8ad7ab9
unknown <hjfender@gmail.com>
2019-07-01 00:02:31 -0500

    Simplified code and added twisting parameter

--- rational_functions_with_weyl_action.py
+++ rational_functions_with_weyl_action.py
@@ -2,7 +2,7 @@ import sys
 from sage.all import *
 from sage.rings.fraction_field_element import make_element
 
-class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionField_generic):
+class RationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionField_generic):
     """
     A class encapsulating the relevant entities used to find a special invariant
     function used to construct a Quadratic Multiple Dirichlet Series, as specified
@@ -17,7 +17,7 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
     fractions, which provides lots of additional functionality.
 
     Note: Because Sage does not allow the square root of a generator of a polynomial
-    ring I implement the parameter variable "q" as "sqrtq" and then use "sqrtq^2"
+    ring I implement the parameter variable "q" as "c" and then use "c^2"
     in the subsequent computations.
 
     Note: Because of numerical instabilities I cannot compute the invariant functions
@@ -27,9 +27,9 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
 
     Example:
         sage: RS = RootSystem(['A',2])
-        sage: F = FieldOfRationalFunctionsWithAction(RS)
+        sage: F = RationalFunctionsWithWeylAction(RS)
         sage: F
-        Fraction Field of Multivariate Polynomial Ring in sqrtq, x1, x2 over Complex Field
+        Fraction Field of Multivariate Polynomial Ring in c, x1, x2 over Complex Field
         with 53 bits of precision endowed with an action by Weyl Group of type ['A', 2]
         (as a matrix group acting on the root lattice)
         sage: F.root_system
@@ -48,29 +48,24 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
         self.lattice = root_system.root_lattice()
         self.weyl_group = self.lattice.weyl_group()
         #construct the function field variables
-        variables = ['sqrtq']
+        variables = ['c']
         for i in range(1,self.lattice.dimension() + 1):
             variables.append('x'+str(i))
         #determine phi dictionary for fast access later
         self.phi = {}
         for w in self.weyl_group.list():
             self.phi[w] = self._phi(w)
-        #create computation field
-        self.CF = PolynomialRing(ZZ, variables).fraction_field()
-        #acted on variables for fast access later
+        #acted on variables for fast access later - initialized in 
         self.acted_variables = {}
-        for i in range(1,self.lattice.dimension() + 1):
-            self.acted_variables["s"+str(i)] = self._act_on_variables("s"+str(i))
-            self.acted_variables["s"+str(i)+"e"+str(i)] = self._act_on_variables("s"+str(i)+"e"+str(i))
         #call parent constructor to create actual field
-        sage.rings.fraction_field.FractionField_generic.__init__(self, PolynomialRing(CC, variables))
+        sage.rings.fraction_field.FractionField_generic.__init__(self, PolynomialRing(ZZ, variables))
 
     def __str__(self):
-        return (super(FieldOfRationalFunctionsWithWeylAction, self).__str__() \
+        return (super(RationalFunctionsWithWeylAction, self).__str__() \
                 + " endowed with an action by " + str(self.weyl_group))
 
     def __repr__(self):
-        return (super(FieldOfRationalFunctionsWithWeylAction, self).__repr__() \
+        return (super(RationalFunctionsWithWeylAction, self).__repr__() \
                 + " endowed with an action by " + str(self.weyl_group))
 
     ################################################################
@@ -84,8 +79,8 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
             sage: F._delta() #F is based on RootSystem(['A',2])
             -(q^2*x0^2*x1^2 - 1)*(q*x0^2 - 1)*(q*x1^2 - 1)
         """
-        p = self.CF(1)
-        q = self.CF('sqrtq')**2
+        p = self(1)
+        q = self('c')**2
         for a in self.lattice.positive_roots():
             cfs = a.coefficients()
             p = p * (1 - q ** sum(cfs) * self._x(2*a))
@@ -107,44 +102,36 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
             -q^4*x0^4*x1^4
         """
         rw = w.reduced_word()
-        variables = self.CF.variable_names()
+        variables = self.variable_names()
         j = 0
-        q = self.CF('sqrtq')**2
+        q = self('c')**2
         if len(rw) == 1:
-            j = -q*self.CF(variables[rw[0]])**2
+            j = -q*self(variables[rw[0]])**2
         else:
             a = sum(self.phi[w])
             cfs = a.coefficients()
             j = (-1)**len(rw) * q**(sum(cfs)) * self._x(2*a)
         return j
 
-    def _f0(self, h):
+    def _f0(self, twisting_parameter = None):
         """
         Compute the f0 part of the invariant polynomial as defined in (3.20) on page 12.
-        Pass in a different function for the twisted version.
+        Pass in a different function for a different version.
         """
-        f0 = self.CF(0)
+        f0 = self(0)
+        one = self.one()
         for w in self.weyl_group.list():
-            g = self._j(w) * self._act(h,w)
+            g = self._j(w) * self.act(one, w, twisting_parameter)
             f0 += g
         return f0
 
-    def _invariant_function(self, h = None):
+    def invariant_function(self, twisting_parameter = None):
         """
         Compute the invariant polynomial as defined in (3.21) on page 12.
-        Pass in a different function for the twisted version.
+        Pass in a different function for a different version.
         """
-        if h is None:
-            h = self.CF.one()
-        function = self._f0(h)/self._delta()
+        function = self._f0(twisting_parameter)/self._delta()
         return function
-
-    def invariant_function(self, h = None):
-        """
-        Compute the invariant polynomial and convert it to the actual ring.
-        """
-        return self(self._invariant_function(h))
-
 
     ################################################################
     #Miscellaneous Rational Functions
@@ -162,10 +149,10 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
             sage: m
             x0^(-2)
         """
-        variables = self.CF.variable_names()
+        variables = self.variable_names()
         p = 1
         for i in range(1,len(variables)):
-            p = p * self.CF(variables[i])**(a.coefficient(i))
+            p = p * self(variables[i])**(a.coefficient(i))
         return p
 
     #################################################################
@@ -224,9 +211,9 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
         sr = self.weyl_group.simple_reflections()
         for j in range(1, len(v)):
             if i == j:
-                w.append(1/(self.CF('sqrtq')**2*v[j]))
+                w.append(1/(self('c')**2*v[j]))
             elif (sr[i] * sr[j])**3 == self.weyl_group.random_element_of_length(0):
-                w.append(v[j]*v[i]*self.CF('sqrtq'))
+                w.append(v[j]*v[i]*self('c'))
             else:
                 w.append(v[j])
         return w
@@ -269,7 +256,7 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
             sage: V.act_on_variables("s1e1s1")
             [x0, -x1]
         """
-        v = [self.CF(x) for x in self.CF.variable_names()]
+        v = [self(x) for x in self.variable_names()]
         for i in range(0,len(pattern_string),2):
             if pattern_string[i] == 's':
                 n = int(pattern_string[i+1])
@@ -290,10 +277,10 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
         """
         Compute the 'c' polynomial defined on page 9.
         """
-        q = self.CF('sqrtq')**2
-        xi = self.CF('x'+str(i))
+        q = self('c')**2
+        xi = self('x'+str(i))
         A = (q*xi - 1)/(q*xi*(1 - xi))
-        B = 1/(self.CF('sqrtq')*xi)
+        B = 1/(self('c')*xi)
         c = (A + B)/2
         return c
 
@@ -301,10 +288,10 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
         """
         Compute the 'd' polynomial defined on page 9.
         """
-        q = self.CF('sqrtq')**2
-        xi = self.CF('x'+str(i))
+        q = self('c')**2
+        xi = self('x'+str(i))
         A = (q*xi - 1)/(q*xi*(1 - xi))
-        B = 1/(self.CF('sqrtq')*xi)
+        B = 1/(self('c')*xi)
         d = (A - B)/2
         return d
 
@@ -316,14 +303,19 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
             sage: f #f is a function in the field based off of ['A', 2]
             1/(x0^4*x1^12)
             sage: WA.simple_action(f,0)
-            -1/2*(1/(sqrtq*x0) + (sqrtq^2*x0 - 1)/(sqrtq^2*(x0 - 1)*x0))/(qsqrtq^4*x0^8*x1^12) + 1/2*(1/(sqrtq*x0) - (sqrtq^2*x0 - 1)/(sqrtq^2*(x0 - 1)*x0))/(sqrtq^4*x0^8*x1^12)
+            -1/2*(1/(c*x0) + (c^2*x0 - 1)/(c^2*(x0 - 1)*x0))/(qc^4*x0^8*x1^12) + 1/2*(1/(c*x0) - (c^2*x0 - 1)/(c^2*(x0 - 1)*x0))/(c^4*x0^8*x1^12)
         """
+        if len(self.acted_variables) == 0:
+            for i in range(1, self.lattice.dimension() + 1):
+                self.acted_variables["s"+str(i)] = self._act_on_variables("s"+str(i))
+                self.acted_variables["s"+str(i)+"e"+str(i)] = self._act_on_variables("s"+str(i)+"e"+str(i))
+                
         vars1 = self.acted_variables["s"+str(i)]
         vars2 = self.acted_variables["s"+str(i)+"e"+str(i)]
         action = self._c(i) * f(vars1) + self._d(i) * f(vars2)
         return action
 
-    def _act(self, f, w):
+    def act(self, f, w, twisting_parameter = None):
         """
         Compute the weyl action extended from the simple action.
 
@@ -332,70 +324,33 @@ class FieldOfRationalFunctionsWithWeylAction(sage.rings.fraction_field.FractionF
             [-1  1]
             [ 0  1]
             sage: f
-            (-x1^2 + x2^2 - 5*sqrtq + x2 + 1)/(-2*sqrtq^2 + 9*sqrtq*x1 - 3*sqrtq*x2 + 4*x1)
+            (-x1^2 + x2^2 - 5*c + x2 + 1)/(-2*c^2 + 9*c*x1 - 3*c*x2 + 4*x1)
             sage: F._act(f,w)
-            (3*sqrtq^11*x1^7*x2^3 + 2*sqrtq^12*x1^6*x2^2 - 3*sqrtq^11*x1^6*x2^3 - 3*sqrtq^11*x1^6*x2^2
-            - 2*sqrtq^10*x1^5*x2^2 - 17*sqrtq^10*x1^5*x2 - 6*sqrtq^9*x1^5*x2^2 - 10*sqrtq^11*x1^4
-            + 17*sqrtq^10*x1^4*x2 + 3*sqrtq^9*x1^5*x2 - 4*sqrtq^8*x1^5*x2^2 + 2*sqrtq^10*x1^4
-            - 3*sqrtq^9*x1^4*x2 + 9*sqrtq^7*x1^4*x2^2 + 10*sqrtq^9*x1^3 + 9*sqrtq^7*x1^4*x2
-            + 4*sqrtq^6*x1^4*x2^2 + 43*sqrtq^8*x1^3 - 9*sqrtq^7*x1^3*x2 + 4*sqrtq^6*x1^4*x2
-            + 11*sqrtq^7*x1^3 - 4*sqrtq^6*x1^3*x2 - 4*sqrtq^6*x1^3 - 3*sqrtq^5*x1^3*x2 - 47*sqrtq^6*x1^2
-            + 3*sqrtq^5*x1^2*x2 - 11*sqrtq^5*x1^2 + 4*sqrtq^4*x1^2 + 2*sqrtq^4*x1 + 9*sqrtq^3*x1
-            + 4*sqrtq^2*x1 - 9*sqrtq - 4)/(-9*sqrtq^12*x1^7*x2^2 + 9*sqrtq^12*x1^6*x2^2 + 4*sqrtq^12*x1^5
-            - 4*sqrtq^12*x1^4 - 36*sqrtq^9*x1^4 + 36*sqrtq^9*x1^3 - 16*sqrtq^8*x1^4 + 16*sqrtq^8*x1^3
-            + 81*sqrtq^6*x1^3 - 81*sqrtq^6*x1^2 + 72*sqrtq^5*x1^3 - 72*sqrtq^5*x1^2 + 16*sqrtq^4*x1^3
-            - 16*sqrtq^4*x1^2)
+            (3*c^11*x1^7*x2^3 + 2*c^12*x1^6*x2^2 - 3*c^11*x1^6*x2^3 - 3*c^11*x1^6*x2^2
+            - 2*c^10*x1^5*x2^2 - 17*c^10*x1^5*x2 - 6*c^9*x1^5*x2^2 - 10*c^11*x1^4
+            + 17*c^10*x1^4*x2 + 3*c^9*x1^5*x2 - 4*c^8*x1^5*x2^2 + 2*c^10*x1^4
+            - 3*c^9*x1^4*x2 + 9*c^7*x1^4*x2^2 + 10*c^9*x1^3 + 9*c^7*x1^4*x2
+            + 4*c^6*x1^4*x2^2 + 43*c^8*x1^3 - 9*c^7*x1^3*x2 + 4*c^6*x1^4*x2
+            + 11*c^7*x1^3 - 4*c^6*x1^3*x2 - 4*c^6*x1^3 - 3*c^5*x1^3*x2 - 47*c^6*x1^2
+            + 3*c^5*x1^2*x2 - 11*c^5*x1^2 + 4*c^4*x1^2 + 2*c^4*x1 + 9*c^3*x1
+            + 4*c^2*x1 - 9*c - 4)/(-9*c^12*x1^7*x2^2 + 9*c^12*x1^6*x2^2 + 4*c^12*x1^5
+            - 4*c^12*x1^4 - 36*c^9*x1^4 + 36*c^9*x1^3 - 16*c^8*x1^4 + 16*c^8*x1^3
+            + 81*c^6*x1^3 - 81*c^6*x1^2 + 72*c^5*x1^3 - 72*c^5*x1^2 + 16*c^4*x1^3
+            - 16*c^4*x1^2)
         """
         word = w.reduced_word()
+
+        if twisting_parameter is not None and len(twisting_parameter) == len(self.gens()) - 1:
+            for i in range(0, len(twisting_parameter)):
+                l = twisting_parameter[i]
+                f = f*self.gen(i+1)**(-l)
+
         for i in word:
             f = self._simple_action(f, i)
+
+        if twisting_parameter is not None and len(twisting_parameter) == len(self.gens()) - 1:
+            for i in range(0, len(twisting_parameter)):
+                l = twisting_parameter[i]
+                f = f*self.gen(i+1)**(l)
+
         return f
-
-    def _act_with_reduction(self, f, w):
-        """
-        Compute the weyl action extended from the simple action but with a reduction step added.
-        """
-        g = self._act(f, w)
-        g.reduce()
-        return g
-
-    ###############################################################
-    #Pretty print and convert to symbolic ring
-    ###############################################################
-    def pretty_print_CF(self, f):
-        """
-        Pretty print a function in the computation field. This converts "sqrtq^2"
-        into "q" so the function looks like it is specified in the paper.
-
-        sage: f = F._invariant_function()
-        sage: F.pretty_print_CF(f)
-        Numerator: x1*x2 - 1
-        Denominator: (q*x1^2*x2^2 - 1)*(x1 - 1)*(x2 - 1)
-        """
-        variables = [self.CF(x) for x in self.CF.variable_names()]
-        variables[0] = sqrt(var('q'))
-        result = f(variables).factor()
-        try:
-            print "Numerator: " + str(result.numerator())
-            print "Denominator: " + str(result.denominators())
-        except:
-            print "Result: " + str(result)
-
-    def pretty_print(self, f):
-        """
-        Pretty print a function in the actual field. This converts "sqrtq^2" 
-        into "q" so the function looks like it is specified in the paper.
-
-        sage: f = F.invariant_function()
-        sage: F.pretty_print(f)
-        Numerator: 1.00000000000000*x1*x2 - 1.00000000000000
-        Denominator: (q*x1^2*x2^2 - 1)*(x1 - 1)*(x2 - 1)
-        """
-        variables = [self(x) for x in self.variable_names()]
-        variables[0] = sqrt(var('q'))
-        result = f(variables).factor()
-        try:
-            print "Numerator: " + str(result.numerator())
-            print "Denominator: " + str(result.denominator())
-        except:
-            print "Result: " + str(result)
